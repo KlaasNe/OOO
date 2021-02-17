4626eff8d40ec38b113a0dad7d858ea0a4b9d052
Klaas <klaas.neyens@gmail.com>
2021-02-17 17:47:51 +0100

    Main and error catching

--- Labo 1/src/ui/ShopAppeke.java
+++ Labo 1/src/ui/ShopAppeke.java
@@ -1,25 +1,30 @@
-//package ui;
-//
-//import domain.Shop2;
-//
-//import javax.swing.*;
-//
-//public class ShopAppeke {
-//    public static void main(String[] args) {
-//        Shop2 shop = new Shop2();
-//
-//        String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
-//        int choice = -1;
-//        while (choice != 0) {
-//            String choiceString = JOptionPane.showInputDialog(menu);
-//            choice = Integer.parseInt(choiceString);
-//            if (choice == 1) {
-//                shop.addProduct(shop);
-//            } else if (choice == 2) {
-//                shop.showProduct(shop);
-//            } else if (choice == 3) {
-//                shop.showPrice(shop);
-//            }
-//        }
-//    }
-//}
+package src.ui;
+
+import src.domain.Shop2;
+
+import javax.swing.*;
+import java.security.KeyException;
+
+public class ShopAppeke {
+    public static void main(String[] args) {
+        Shop2 shop = new Shop2();
+
+        String menu = "1. Add product\n2. Show product\n3. Show rental price\n\n0. Quit";
+        int choice = -1;
+        while (choice != 0) {
+            String choiceString = JOptionPane.showInputDialog(menu);
+            choice = Integer.parseInt(choiceString);
+            try {
+                if (choice == 1) {
+                    shop.addProduct();
+                } else if (choice == 2) {
+                    shop.showProduct();
+                } else if (choice == 3) {
+                    shop.showPrice();
+                }
+            } catch (KeyException k) {
+                System.out.println("id does not exist"); //TODO
+            }
+        }
+    }
+}
