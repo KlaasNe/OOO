4b819b796492cc563bfbec216c4e9d388690e496
Klaas <klaas.neyens@gmail.com>
2021-02-24 10:18:40 +0100

    Spiegeling

--- Labo 2/src/domain/Spiegeling.java
+++ Labo 2/src/domain/Spiegeling.java
@@ -1,4 +1,26 @@
 package domain;
 
 public class Spiegeling {
+
+    private final String text;
+
+    public Spiegeling(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public String spiegel() {
+        return StringFormatter.reverseString(this.getText());
+    }
+}
+
+class StringFormatter {
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
 }
